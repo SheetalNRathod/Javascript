3eec9bfa4d6deac6666525366ae90428432049bc
SheetalNRathod <127586421+SheetalNRathod@users.noreply.github.com>
2023-03-31 18:06:04 +0530

    arry.js

---
+++ array.js
@@ -0,0 +1,75 @@
+//========array as object========================//
+    //----------------------------------------------------------------------------------------------//
+//  let arr = [10, 20, "thirty", 40, " 50", "60", 70, 80, 90, 100]; 
+ 
+// console.log(typeof (arr));   //object  
+// console.log(arr);    //full array  value  
+ 
+// console.log(arr[2]); //thirty  
+                     //thirty  
+//   
+// console.log(arr["3"]);       //thirty  
+ 
+// console.log(arr[3.0]);//40  
+// arr[3.2]="3.2";  
+//  console.log(arr[3.2]);   //3.2  
+//   console.log( arr);  
+ 
+// arr["name"]="skill";  
+// console.log(arr["name"]);   //skill  
+// console.log(arr.name);  
+ 
+// let arry=[10,20,"thirty",40,[100,"thousand"],"60",70,80,90,100];  
+// console.log(arry[4][1][3]);  
+ 
+// console.log(arry[4][0]);  
+// let twoDarr=[[10,20,30],[40,50,60],[70,80,90]];  
+//  console.log(twoDarr);  
+//   console.log(twoDarr[1][1]);  
+// console.log(twoDarr.length);  
+ 
+ 
+ 
+// let arry = [10, 20, 30, 40, 50, 60, 70]; 
+// console.log(arry[0],arry[1],arry[2],arry[3],arry[4],arry[5],arry[6]);  
+// console.log(arry[-1]); 
+ 
+// ================================================================================================================================ 
+ 
+//DELETE 
+//Delete The Particular 
+ 
+// let arr2 = [10,20,30,40,50,60,70,80,90,100] 
+// console.log (arr2.length) 
+//  arr2[19]=200 
+// console.log (arr2) 
+// delete arr2 [4] 
+// delete arr2 [6] 
+// delete arr2 [8] 
+// console.log (arr2) 
+// arr2[4]=undefined 
+// arr2[5]=undefined 
+// delete arr2[7] 
+// delete arr2[9] 
+// arr2 [8] = undefined 
+// console.log (arr2) 
+ 
+// let arr2 = [20,30,40]; 
+// console.log (arr2) 
+// arr2.push (50) 
+// console.log (arr2) 
+// console.log (arr2.length) 
+// arr2.pop() 
+// console.log (arr2) 
+// console.log (arr2.length) 
+ 
+let arr2 = [20, 30, 40]; 
+// arr2.unshift(10) 
+// arr2.unshift(9) 
+// arr2.unshift(8) 
+// console.log(arr2) 
+// console.log(arr2.length) 
+// arr2.shift() 
+console.log(arr2) 
+console.log(arr2.length)
+
