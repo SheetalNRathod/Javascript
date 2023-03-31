929b1a228b6cccb439535ae51ba2ced862e56e8f
SheetalNRathod <127586421+SheetalNRathod@users.noreply.github.com>
2023-03-31 18:11:56 +0530

    namedfunction_js

---
+++ namedfunction1.js
@@ -0,0 +1,71 @@
+//date  27-01-2023
+
+// function fun_one(arg)
+// {
+//     //arg()
+//     console.log(arg());
+// }
+// function test()
+// {   
+
+//     console.log("test");
+   
+// }
+// fun_one(test);
+
+////////////////////////using before-after/////////////////////////////////////
+
+// function fun_one(arg)
+// {
+//     //arg()
+//     console.log("before function");
+//     console.log(arg());
+// }
+// function test()
+// {   
+
+//     console.log("after function");
+
+// }
+// fun_one(test);
+
+// function fun_one(arg1,arg2,arg3)
+// {
+    
+//     console.log("before function.............");
+//     //  console.log(arg1,arg2,arg3)  //output=[Function: fun_two] [Function: fun_three] [Function: fun_four]
+
+//     console.log(arg1(),arg2(),arg3());
+// }
+// function fun_two()
+// {   
+
+//     // console.log("after function");
+//      console.log("hello 1....");
+//     return "hello 1....";
+
+// }
+// function fun_three()
+// {   
+
+//     // console.log("after function");
+//     console.log("hello 2....");
+//     return "hello 2....";
+// }
+// function fun_four()
+// {   
+
+//     // console.log("after function");
+//      console.log("hello 3....");
+//     return "hello 3....";
+// }
+// fun_one(fun_two,fun_three,fun_four);
+
+//------------------------------------------------------------------------
+
+
+
+  
+
+
+
